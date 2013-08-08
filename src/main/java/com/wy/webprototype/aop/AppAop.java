ab0ea73d089a5cca7ab3d2f143d792982cab7144
whitman.yang <whitman.yang@gmail.com>
2013-08-08 16:25:08 +0800

    add more AOP examples (around, after pointcut weaving)

--- src/main/java/com/wy/webprototype/aop/AppAop.java
+++ src/main/java/com/wy/webprototype/aop/AppAop.java
@@ -1,42 +1,47 @@
 package com.wy.webprototype.aop;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import static com.wy.webprototype.util.ComLogger.info;
+import static com.wy.webprototype.util.ComLogger.isInfo;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 
 public class AppAop {
-	
-	private Logger logger = LoggerFactory.getLogger(AppAop.class);
-	
-	public void doBefore() {  
-		logger.info("Entering method... ");
-	}  
-	
-	/**
-	public void doAfter(JoinPoint jp) {  
-        System.out.println("log Ending method: "  
-                + jp.getTarget().getClass().getName() + "."  
-                + jp.getSignature().getName());  
-    }  
-  
-    public Object doAround(ProceedingJoinPoint pjp) throws Throwable {  
-        long time = System.currentTimeMillis();  
-        Object retVal = pjp.proceed();  
-        time = System.currentTimeMillis() - time;  
-        System.out.println("process time: " + time + " ms");  
-        return retVal;  
-    }  
-  
-    public void doBefore(JoinPoint jp) {  
-        System.out.println("log Begining method: "  
-                + jp.getTarget().getClass().getName() + "."  
-                + jp.getSignature().getName());  
-    }  
-  
-    public void doThrowing(JoinPoint jp, Throwable ex) {  
-        System.out.println("method " + jp.getTarget().getClass().getName()  
-                + "." + jp.getSignature().getName() + " throw exception");  
-        System.out.println(ex.getMessage());  
-    }  
-    */
+
+	private boolean isMonitorRunningTime;
+
+	public boolean isMonitorRunningTime() {
+		return isMonitorRunningTime;
+	}
+
+	public void setIsMonitorRunningTime(boolean isMonitorRunningTime) {
+		this.isMonitorRunningTime = isMonitorRunningTime;
+	}
+
+	public void doBefore(JoinPoint jp) {
+		if (isInfo()) {
+			info("Entering method - " + jp.getTarget().getClass().getName()
+					+ "." + jp.getSignature().getName());
+		}
+	}
+
+	public void doAfter(JoinPoint jp) {
+		if (isInfo()) {
+			info("Leaving method - " + jp.getTarget().getClass().getName()
+					+ "." + jp.getSignature().getName());
+		}
+	}
+
+	public Object doAround(ProceedingJoinPoint pjp) throws Throwable {
+		if (isMonitorRunningTime && isInfo()) {
+			long time = System.currentTimeMillis();
+			Object retVal = pjp.proceed();
+			time = System.currentTimeMillis() - time;
+			info("Process time: " + time + " ms");
+			return retVal;
+		} else {
+			return pjp.proceed();
+		}
+	}
 
 }
